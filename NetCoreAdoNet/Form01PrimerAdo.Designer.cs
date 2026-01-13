7e49261d68e5b89849f24f6f982a1ef629d1a493
Kevbast <kevbast936@gmail.com>
2026-01-13 13:42:47 +0100

    Add project files.

---
+++ NetCoreAdoNet/Form01PrimerAdo.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace NetCoreAdoNet
+{
+    partial class Form01PrimerAdo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnConectar = new Button();
+            btnDesconectar = new Button();
+            btnRead = new Button();
+            label1 = new Label();
+            lstApellidos = new ListBox();
+            lstColumnas = new ListBox();
+            label2 = new Label();
+            lstTipos = new ListBox();
+            label3 = new Label();
+            lblConexion = new Label();
+            SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            btnConectar.Location = new Point(24, 68);
+            btnConectar.Name = "btnConectar";
+            btnConectar.Size = new Size(75, 23);
+            btnConectar.TabIndex = 0;
+            btnConectar.Text = "Conectar";
+            btnConectar.UseVisualStyleBackColor = true;
+            btnConectar.Click += btnConectar_Click;
+            // 
+            // btnDesconectar
+            // 
+            btnDesconectar.Location = new Point(24, 121);
+            btnDesconectar.Name = "btnDesconectar";
+            btnDesconectar.Size = new Size(75, 23);
+            btnDesconectar.TabIndex = 1;
+            btnDesconectar.Text = "Desconectar";
+            btnDesconectar.UseVisualStyleBackColor = true;
+            btnDesconectar.Click += btnDesconectar_Click;
+            // 
+            // btnRead
+            // 
+            btnRead.Location = new Point(24, 164);
+            btnRead.Name = "btnRead";
+            btnRead.Size = new Size(75, 23);
+            btnRead.TabIndex = 2;
+            btnRead.Text = "Read";
+            btnRead.UseVisualStyleBackColor = true;
+            btnRead.Click += btnRead_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(157, 40);
+            label1.Name = "label1";
+            label1.Size = new Size(56, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Apellidos";
+            // 
+            // lstApellidos
+            // 
+            lstApellidos.FormattingEnabled = true;
+            lstApellidos.Location = new Point(130, 68);
+            lstApellidos.Name = "lstApellidos";
+            lstApellidos.Size = new Size(163, 319);
+            lstApellidos.TabIndex = 4;
+            // 
+            // lstColumnas
+            // 
+            lstColumnas.FormattingEnabled = true;
+            lstColumnas.Location = new Point(319, 65);
+            lstColumnas.Name = "lstColumnas";
+            lstColumnas.Size = new Size(163, 319);
+            lstColumnas.TabIndex = 6;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(346, 37);
+            label2.Name = "label2";
+            label2.Size = new Size(61, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Columnas";
+            // 
+            // lstTipos
+            // 
+            lstTipos.FormattingEnabled = true;
+            lstTipos.Location = new Point(530, 65);
+            lstTipos.Name = "lstTipos";
+            lstTipos.Size = new Size(163, 319);
+            lstTipos.TabIndex = 8;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(557, 37);
+            label3.Name = "label3";
+            label3.Size = new Size(83, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Tipos de datos";
+            // 
+            // lblConexion
+            // 
+            lblConexion.AutoSize = true;
+            lblConexion.Location = new Point(28, 426);
+            lblConexion.Name = "lblConexion";
+            lblConexion.Size = new Size(71, 15);
+            lblConexion.TabIndex = 9;
+            lblConexion.Text = "lblConexion";
+            // 
+            // Form01PrimerAdo
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblConexion);
+            Controls.Add(lstTipos);
+            Controls.Add(label3);
+            Controls.Add(lstColumnas);
+            Controls.Add(label2);
+            Controls.Add(lstApellidos);
+            Controls.Add(label1);
+            Controls.Add(btnRead);
+            Controls.Add(btnDesconectar);
+            Controls.Add(btnConectar);
+            Name = "Form01PrimerAdo";
+            Text = "Form01PrimerAdo";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnConectar;
+        private Button btnDesconectar;
+        private Button btnRead;
+        private Label label1;
+        private ListBox lstApellidos;
+        private ListBox lstColumnas;
+        private Label label2;
+        private ListBox lstTipos;
+        private Label label3;
+        private Label lblConexion;
+    }
+}
---
+++ NetCoreAdoNet/Form01PrimerAdo.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NetCoreAdoNet
+{
+    public partial class Form01PrimerAdo : Form
+    {
+        //SOTA,CABALLO,REY siempre lo mismo
+        SqlConnection cn;
+        SqlCommand com;
+        SqlDataReader reader;
+        string connectionString;
+
+        public Form01PrimerAdo()
+        {
+            InitializeComponent();
+            //COPIAMOS LA CONECTION STRING 
+            this.connectionString = @"Data Source=LOCALHOST\DEVELOPER;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA;Trust Server Certificate=True";
+            this.cn = new SqlConnection(this.connectionString);
+            this.com = new SqlCommand();
+            //recuperamos la conexión
+            this.cn.StateChange += Cn_StateChange;
+        }
+
+        private void Cn_StateChange(object sender, StateChangeEventArgs e)
+        {
+            this.lblConexion.Text = "La conexion está pasando de " + e.OriginalState + " a " + e.CurrentState;
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            //this.cn.ConnectionString = this.connectionString; lo ponemos en el inialice
+            //CONTROL DE EXCEPCIONES
+            try
+            {
+                if (this.cn.State == ConnectionState.Closed)
+                {
+
+                    this.cn.Open();
+                    this.lblConexion.BackColor = Color.LightGreen;
+                    //this.lblConexion.Text = "Base de Datos conectada";
+                }
+            }
+            catch (SqlException ex)
+            {
+                this.lblConexion.Text = "Error con la conexion de la BD" + ex;
+            }
+            catch (Exception ex)
+            {
+                this.lblConexion.Text = "Error: " + ex;
+            }
+
+
+        }
+
+        private void btnDesconectar_Click(object sender, EventArgs e)
+        {
+            this.cn.Close();
+            this.lblConexion.BackColor = Color.LightCoral;
+            //this.lblConexion.Text = "Base de Datos desconectada";
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {//PARA LEER REGISTROS LA CONEXIÓN SIEMPRE OPEN
+            string sql="select * from EMP";
+            //INDICAMOS LA CONEXION DEL COMAND
+            this.com.Connection = this.cn;//la conexión a utilizar
+            //TIPO DE CONSULTA A REALIZAR 
+            this.com.CommandType = CommandType.Text;
+            //INCLUIMOS LA CONSULTA EN EL COMMAND
+            this.com.CommandText = sql;
+            //AQUÍ DEBERÍAMOS ABRIR LA CONEXIÓN,PERO COMO ESTÁ SEPARADO NO LO HARÍAMOS POR AHORA
+            //EJECUTAMOS LA CONSULTA.COMO ES UN SELECT
+            //UTILIZAMOS ExecuteReader() QUE DEVUELVE UN DataReader
+            this.reader = this.com.ExecuteReader();
+            //LEEMOS LA PRIMERA COLUMNA DE LA CONSULTA
+            string columna = this.reader.GetName(0);//dame el nombre de la primera columna
+            //LEEMOS EL TIPO DE DATO DE LA PRIMERA COLUMNA
+            string tipo = this.reader.GetDataTypeName(0);
+            this.lstColumnas.Items.Add(columna);
+            this.lstTipos.Items.Add(tipo);
+            //SIEMPRE DEBEMOS SALIR,CERRAR
+            this.reader.Close();
+        }
+    }
+}
